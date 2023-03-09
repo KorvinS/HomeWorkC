43283bd780d716e0495d374e526df5cbab607a6a
KorvinS <info@bpm-control.ru>
2023-03-09 17:44:56 +0300

    Решение первой задачки. Какое из введенных числел больше - скорректировал код. Лишнее убрал

--- HM1/Program.cs
+++ HM1/Program.cs
@@ -3,8 +3,6 @@ Console.Write("Введите 1ое число ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2ое число ");
 int b = Convert.ToInt32(Console.ReadLine());
-int max = 0;
-int min = 0;
 if (a > b)
 {
 Console.WriteLine("Первое число " + a + " больше чем второе " + b);
