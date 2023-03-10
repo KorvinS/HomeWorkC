10fb63596205a63913cab258532cd165186636c4
KorvinS <info@bpm-control.ru>
2023-03-10 11:35:09 +0300

    Ошибка - не 4ой а 3ей задачи

--- HM3/Program.cs
+++ HM3/Program.cs
@@ -1,7 +1,6 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите число:");
 int a = Convert.ToInt32(Console.ReadLine());
-
 if (a % 2 == 1)
 {
     Console.WriteLine("Число " + a + " является: НЕЧЁТНЫМ");
