8ac1f8d619f742bd8b596eac1b2d1f03f449478f
KorvinS <info@bpm-control.ru>
2023-03-13 00:08:24 +0300

    nan

--- HM3/Program.cs
+++ HM3/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите число:");
 int a = Convert.ToInt32(Console.ReadLine());
-if (a % 2 == 1) Console.WriteLine("Число " + a + " является: НЕЧЁТНЫМ");
-else Console.WriteLine("Число " + a + "является: ЧЁТНЫМ");
+if (a % 2 == 0) Console.WriteLine("Число " + a + " является: ЧЁТНЫМ");
+else Console.WriteLine("Число " + a + "является: НЕ ЧЁТНЫМ");
