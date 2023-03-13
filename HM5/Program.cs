c399337084d74f7ca8ee71906981987caa064b5a
KorvinS <info@bpm-control.ru>
2023-03-13 14:06:38 +0300

    Реализация задачи не используя массив

--- HM5/Program.cs
+++ HM5/Program.cs
@@ -1,8 +1,12 @@
-﻿Console.Clear();
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
 Console.WriteLine("Введите 3х значное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int n1 = n / 100;
 int n2 = n / 10;
-
-
-Console.WriteLine(n2);
+Console.WriteLine(n2 - n1 * 10);
