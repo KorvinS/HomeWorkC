5194e3466885b31196322d3327bc6b540bdbce58
KorvinS <info@bpm-control.ru>
2023-03-13 14:33:43 +0300

    Подправил код

--- HM5/Program.cs
+++ HM5/Program.cs
@@ -7,6 +7,13 @@
 Console.Clear();
 Console.WriteLine("Введите 3х значное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 100 || n > 1000)
+{
+    Console.WriteLine("У Вас был шанс! Прощайте!");
+    return;
+}
+
 int n1 = n / 100;
 int n2 = n / 10;
 Console.WriteLine(n2 - n1 * 10);
