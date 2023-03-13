fb49be1d1b549a04bbb78912bbe2b0c5f3d31a66
KorvinS <info@bpm-control.ru>
2023-03-13 15:49:11 +0300

    Простое решение вычисления дня недели Выходной или Будний день

---
+++ HM7/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите число соответствующее дню недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 1 && n <= 5)
+{
+    Console.WriteLine("Будний день!");
+    return;
+}
+else if (n >= 6 && n <= 7)
+{
+    Console.WriteLine("Выходной!");
+    return;
+}
+
+else
+{
+    Console.WriteLine("Мне нечего сказать");
+    return;
+}
+
+
+
+
