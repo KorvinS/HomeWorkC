b670524eb392921009f827e6ed6637f32571d0e0
KorvinS <info@bpm-control.ru>
2023-03-20 14:16:18 +0300

    Сокращение папок

--- HM3/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.Clear();
-Console.WriteLine("Введите число:");
-int a = Convert.ToInt32(Console.ReadLine());
-if (a % 2 == 0) Console.WriteLine("Число " + a + " является: ЧЁТНЫМ");
-else Console.WriteLine("Число " + a + "является: НЕ ЧЁТНЫМ");
