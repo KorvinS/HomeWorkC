bbbbe7e7fbb3f5f92d0f6628f27b7986b8bf61b5
KorvinS <info@bpm-control.ru>
2023-03-21 11:52:48 +0300

    Задачу решали ранее

---
+++ HM27/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string s = Console.ReadLine()!;
+int sum = 0;
+for (int i = 0; i < s.Length; i++)
+    sum = sum + int.Parse(s[i].ToString());
+Console.WriteLine(sum);
