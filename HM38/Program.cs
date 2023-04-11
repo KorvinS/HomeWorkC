41041f2b943c28f612e67a21abfa38dbc5d5ca72
KorvinS <info@bpm-control.ru>
2023-04-11 12:21:26 +0300

    ...

---
+++ HM38/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.03 7.85 22.13 2.05 78.48] -> 76.43
+
+
+Console.Clear();
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+ArrRnd(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int z = 0; z < numbers.Length; z++)
+{
+    if (numbers[z] > max)
+    {
+        max = numbers[z];
+    }
+    if (numbers[z] < min)
+    {
+        min = numbers[z];
+    }
+}
+
+Console.WriteLine($"Всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void ArrRnd(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 100;
+    }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
