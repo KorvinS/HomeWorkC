5e044748ca9e0cc4a422340f5f88613d033e4c6d
KorvinS <info@bpm-control.ru>
2023-04-18 14:15:30 +0300

    Исправил ошибку... Нихрена не понял и удивился комментарию! НИОЧЕМ!

--- HM36/Program.cs
+++ HM36/Program.cs
@@ -5,33 +5,31 @@
 // [-4, -6, 89, 6] -> 0
 
 Console.Clear();
-Console.WriteLine("Введите размер массива: ");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
-ArrRnd(numbers);
-Console.WriteLine("Вот наш массив: ");
-PrintArray(numbers);
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
+
+void mas(int a)
+{
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = new Random().Next(1,9);
+Console.Write(randomArray[i] + " ");
+}
+
+}
+
+int kol(int[] randomArray)
+{
 int sum = 0;
-
-for (int z = 0; z < numbers.Length; z += 2)
-    sum = sum + numbers[z];
-
-Console.WriteLine($"Всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
-
-void ArrRnd(int[] numbers)
+int i = 0;
+while (i < randomArray.Length)
 {
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        numbers[i] = new Random().Next(1, 10);
-    }
+sum = sum + randomArray[i];
+i = i + 2;
 }
-void PrintArray(int[] numbers)
-{
-    Console.Write("[ ");
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        Console.Write(numbers[i] + " ");
-    }
-    Console.Write("]");
-    Console.WriteLine();
+return sum;
 }
+
+mas(a);
+Console.Write($"Cумма элементов, стоящих на нечётных позициях: {kol(randomArray)}");
